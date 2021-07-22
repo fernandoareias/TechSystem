2c7ece4be6ce1c6ef016ec737c3282bb0bc62352
Fernando <nando.calheirosx@gmail.com>
2021-07-22 17:06:06 -0300

    CRUD Generic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BOBTechSystem.Repository;
+using Microsoft.Data.SqlClient;
+using BOBTechSystem.Models;
 
 namespace BOBTechSystem
 {
@@ -6,7 +8,22 @@ namespace BOBTechSystem
    {
       static void Main(string[] args)
       {
+         const string CONNECTSTRING = "Server=localhost;Database=BOBTech;User Id=sa;Password=Nando@37074957;";
 
+         using (var connection = new SqlConnection(CONNECTSTRING))
+         {
+            var repository = new Repository<Employee>(connection);
+
+            var employee = repository.GetAll();
+
+            foreach (var item in employee)
+            {
+               System.Console.WriteLine(item.Name);
+            }
+
+            System.Console.WriteLine();
+         }
       }
    }
 }
+
--- Repositorys/Repository.cs
+++ Repositorys/Repository.cs
@@ -1,24 +1,25 @@
-
-
 using System;
 using System.Collections;
-using BOBTechSystem.Models;
+using System.Collections.Generic;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 namespace BOBTechSystem.Repository
 {
+   // Função generica para ralização do CRUD
 
    public class Repository<T> where T : class
    {
-      public SqlConnection _connection { get; set; }
-      public Repository(SqlConnection connection) => _connection = _connection;
-
-      public IEnumerable GetAll() => _connection.GetAll<T>();
+      private readonly SqlConnection _connection;
+      public Repository(SqlConnection connection) => _connection = connection;
+      public IEnumerable<T> GetAll() => _connection.GetAll<T>();
 
       public T Get(Guid id) => _connection.Get<T>(id);
-
       public void Set(T model) => _connection.Insert<T>(model);
+      public void Update(T model) => _connection.Update<T>(model);
+      public void Update(Guid id) => _connection.Update<T>(_connection.Get<T>(id));
+      public void Delete(T model) => _connection.Delete<T>(model);
+      public void Delete(Guid id) => _connection.Delete<T>(_connection.Get<T>(id));
 
    }
 }
