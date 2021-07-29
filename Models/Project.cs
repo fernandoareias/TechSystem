193d1a7c3cf6dd5acf6faf90b12f5d07702bbc63
Fernando <nando.calheirosx@gmail.com>
2021-07-29 15:41:40 -0300

    Include prop Step/Minutes in Project class

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,15 +9,22 @@ namespace TechSystem.Models
 
    public class Project
    {
+      public Project()
+      {
+         InitialDate = DateTime.Now;
+      }
       [Key]
       public int Id { get; set; }
+
       [Required(ErrorMessage = "Esse campo é obrigatório")]
       [MaxLength(80, ErrorMessage = "Esse campo deve conter de 2 a 80 caractéres")]
       public string Name { get; set; }
+
       public DateTime InitialDate { get; set; }
-      /*
-            [Write(false)]
-            public List<Project> Projects { get; set; }
-            */
+
+      public List<ProjectStep> Steps { get; set; }
+
+      public List<ProjectMinute> Minute { get; set; }
+
    }
 }
