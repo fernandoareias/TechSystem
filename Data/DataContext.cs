19b658713c63a25d06854f5704e9c55c005fd02c
Fernando <nando.calheirosx@gmail.com>
2021-07-30 10:43:14 -0300

    Temp rm

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -14,9 +14,7 @@ namespace TechSystem.Data
       public DbSet<Employee> Employees { get; set; }
       public DbSet<Dependent> Dependents { get; set; }
       public DbSet<Department> Department { get; set; }
-      public DbSet<DepartmentHasProject> DepartMentHasProjects { get; set; }
       public DbSet<Project> Projects { get; set; }
-      public DbSet<ProjectHasEmployee> ProjectHasEmployees { get; set; }
       public DbSet<ProjectMinute> ProjectMinutes { get; set; }
       public DbSet<ProjectStep> ProjectSteps { get; set; }
 
