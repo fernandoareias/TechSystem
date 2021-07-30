0e826334164f1487d27b254610d7e691dbac7a80
Fernando <nando.calheirosx@gmail.com>
2021-07-30 10:22:29 -0300

    nova rota

--- Controllers/DependentControllers.cs
+++ Controllers/DependentControllers.cs
@@ -9,19 +9,21 @@ using TechSystem.Models;
 
 namespace TechSystem.Controllers
 {
-   [Route("v1/dependent")] // https://localhost:5001/v1/dependent
+   [Route("v1/employees")] // https://localhost:5001/v1/employee/dependent
    public class DependentController : ControllerBase
    {
 
+
+
       [HttpGet]
-      [Route("")] // https://localhost:5001/v1/dependent
-      public async Task<ActionResult<List<Dependent>>> Get([FromServices] DataContext context)
+      [Route("{employeeId:int}/dependents")] // https://localhost:5001/v1/employee/1/dependent
+      public async Task<ActionResult<List<Dependent>>> Get(int employeeId, [FromServices] DataContext context)
       {
          try
          {
-            var dependents = await context.Dependents.AsNoTracking().ToListAsync();
+            var dependents = await context.Dependents.AsNoTracking().Where(y => y.EmployeeId == employeeId).ToListAsync();
             if (dependents == null)
-               return NotFound(new { message = "Não há dependentes registrado" });
+               return NotFound(new { message = "Esse funcionário não possui dependentes" });
             return Ok(dependents);
          }
          catch (System.Exception e)
@@ -31,33 +33,45 @@ namespace TechSystem.Controllers
          }
       }
 
+
       [HttpGet]
-      [Route("{id:int}")] // https://localhost:5001/v1/dependent/1
-      public async Task<ActionResult<Dependent>> GetById(int id, [FromServices] DataContext context)
+      [Route("{employeeId:int}/dependents/{dependentsId}")] // https://localhost:5001/v1/employee/1/dependent
+      public async Task<ActionResult<Dependent>> GetById(int employeeId, int dependentsId, [FromServices] DataContext context)
       {
          try
          {
-            var dependet = await context.Dependents.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
-            if (dependet == null)
-               return NotFound(new { message = "Não foi possivel encontrar o dependente" });
-            return Ok(dependet);
+            var dependents = await context.Employees.Include(x => x.Dependents).FirstOrDefaultAsync(y => y.Id == employeeId);
+            if (dependents == null)
+               return NotFound(new { message = "Esse funcionário não possui dependentes" });
+
+            foreach (var item in dependents.Dependents)
+               if (item.Id == dependentsId)
+                  return Ok(item);
+
+            return NotFound();
          }
-         catch (System.Exception)
+         catch (System.Exception e)
          {
 
-            return BadRequest(new { message = "Desculpe ocorreu um erro. Por favor, tente novamente mais tarde" });
+            return BadRequest(new { message = $"{e.Message}" });
          }
       }
 
-
-
       [HttpPost]
-      [Route("")] // https://localhost:5001/v1/dependent/
+      [Route("{employeeId:int}/dependents")] // https://localhost:5001/v1/employee/1/dependent
 
-      public async Task<ActionResult<Dependent>> Post([FromServices] DataContext context, [FromBody] Dependent model)
+      public async Task<ActionResult<Dependent>> Post(int employeeId, [FromServices] DataContext context, [FromBody] Dependent model)
       {
          if (!ModelState.IsValid)
             return BadRequest(ModelState);
+         var temp = await context.Employees.AsNoTracking().FirstOrDefaultAsync(x => x.Id == employeeId);
+
+         if (temp == null)
+            return NotFound();
+
+         if (model.EmployeeId != employeeId)
+            return NotFound();
+
          try
          {
             context.Dependents.Add(model);
@@ -71,15 +85,17 @@ namespace TechSystem.Controllers
       }
 
       [HttpPut]
-      [Route("{id:int}")] // https://localhost:5001/v1/dependent/1
+      [Route("{employeeId:int}/dependents/{dependentId}")] // https://localhost:5001/v1/employee/3/dependent/1
 
-      public async Task<ActionResult<Dependent>> Get(int id, [FromServices] DataContext context, [FromBody] Dependent model)
+      public async Task<ActionResult<Dependent>> Get(int employeeId, int dependentId, [FromServices] DataContext context, [FromBody] Dependent model)
       {
          if (!ModelState.IsValid)
             return BadRequest(ModelState);
-
-         if (model.Id != id)
+         var temp = await context.Employees.AsNoTracking().FirstOrDefaultAsync(x => x.Id == employeeId);
+         if (model.Id != dependentId)
             return NotFound(new { message = "Não foi possivel encontrar o dependente" });
+
+
          try
          {
             context.Entry<Dependent>(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
@@ -94,11 +110,11 @@ namespace TechSystem.Controllers
       }
 
       [HttpDelete]
-      [Route("{id:int}")] // https://localhost:5001/v1/dependent/2
+      [Route("{employeeId:int}/dependents/{dependentId}")] // https://localhost:5001/v1/employee/dependent/2
 
-      public async Task<ActionResult<Dependent>> Delete(int id, [FromServices] DataContext context)
+      public async Task<ActionResult<Dependent>> Delete(int dependentId, [FromServices] DataContext context)
       {
-         var dependent = await context.Dependents.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+         var dependent = await context.Dependents.AsNoTracking().FirstOrDefaultAsync(x => x.Id == dependentId);
          if (dependent == null)
             return NotFound();
 
@@ -114,5 +130,7 @@ namespace TechSystem.Controllers
             return BadRequest(new { message = "Desculpe ocorreu um erro. Por favor, tente novamente mais tarde" });
          }
       }
+
+
    }
 }
