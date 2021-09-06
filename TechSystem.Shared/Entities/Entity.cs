ead166dd6fd44430c98ac0f2e63eade6bc620cb0
fernandoareias <nando.calheirosx@gmail.com>
2021-09-06 19:20:09 -0300

    Entity Base

---
+++ TechSystem.Shared/Entities/Entity.cs
@@ -0,0 +1,16 @@
+using System;
+using Flunt.Notifications;
+
+namespace TechSystem.Shared.Entities
+{
+    public abstract class Entity : Notifiable<Notification>, IEquatable<Entity>
+    {
+        // Gera um novo ID
+        public Entity() => Id = System.Guid.NewGuid();
+
+        public System.Guid Id { get; private set; }
+
+        // Compata os IDs
+        public bool Equals(Entity other) => Id == other.Id;
+    }
+}
