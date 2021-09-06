28d6e6158f8242f7453f816aa462cf27e9bb6349
fernandoareias <nando.calheirosx@gmail.com>
2021-09-06 19:21:33 -0300

    Enums for Genders

---
+++ TechSystem.Domain/Enums/EGender.cs
@@ -0,0 +1,15 @@
+// Generos
+
+namespace TechSystem.Domain.Enums
+{
+    public enum EGender
+    {
+        Female = 1,
+        Male = 2,
+        Transgender = 3,
+        Neutral = 4,
+        NonBinary = 5,
+        Pangender = 6,
+        Other = 7
+    }
+}
