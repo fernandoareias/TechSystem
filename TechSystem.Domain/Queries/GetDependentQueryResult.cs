68185f173e8f863a50f91ba1337df6c3cf488f1c
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 18:22:35 -0300

    Test Dependent query result

---
+++ TechSystem.Domain/Queries/GetDependentQueryResult.cs
@@ -0,0 +1,13 @@
+
+
+
+
+namespace TechSystem.Domain.Queries
+{
+    public class GetDependentQueryResult
+    {
+        public System.Guid Id { get; set; }
+        public string Name { get; set; }
+        public System.Guid EmployeeId { get; set; }
+    }
+}
