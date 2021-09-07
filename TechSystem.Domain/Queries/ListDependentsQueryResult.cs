9880f91b79757f56c9aedc9df9e993844ffb3caf
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 11:24:06 -0300

    Model for read in DB

---
+++ TechSystem.Domain/Queries/ListDependentsQueryResult.cs
@@ -0,0 +1,10 @@
+
+namespace TechSystem.Domain.Queries
+{
+    public class ListDependentsQueryResults
+    {
+        public System.Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
