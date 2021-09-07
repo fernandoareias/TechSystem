2fa696de1666e28df6ceeb69a35aae8111db9dc8
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 18:20:19 -0300

    Temporary Test

--- TechSystem.Domain/Commands/Contracts/ICommandHandle.cs
+++ TechSystem.Domain/Commands/Contracts/ICommandHandle.cs
@@ -5,5 +5,6 @@ namespace BoBStore.Shared.Commands
     public interface ICommandHandler<T> where T : ICommands
     {
         ICommandResults Handler(T Command);
+        ICommandResults Handler(T Command, System.Guid Id);
     }
 }
