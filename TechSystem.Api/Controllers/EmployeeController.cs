ff86bca7827aa0f9e315e12983b2c0bf0d76c9bc
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 13:21:36 -0300

    Add Get() and GetListEmployeesAndDependents()

--- TechSystem.Api/Controllers/EmployeeController.cs
+++ TechSystem.Api/Controllers/EmployeeController.cs
@@ -4,6 +4,8 @@
 using System.Collections.Generic;
 using BoBStore.Shared;
 using Microsoft.AspNetCore.Mvc;
+using TechSystem.Domain.Commands;
+using TechSystem.Domain.Commands.EmployeeCommands.Inputs;
 using TechSystem.Domain.Handlers;
 using TechSystem.Domain.Queries;
 using TechSystem.Domain.Repositories.Contracts;
@@ -25,18 +27,30 @@ namespace TechSystem.Api.Controllers
 
 
         [HttpGet]
-        [Route("employees")]
+        [Route("v1/employees/dependents")]
+        public IEnumerable<ListEmployeeAndDependentsQueryResults> GetEmployeesAndDependents()
+        {
+            return _repository.GetListEmployeesAndDependents();
+        }
+
+        [HttpGet]
+        [Route("v1/employees")]
         public IEnumerable<ListEmployeeQueryResults> Get()
         {
             return _repository.Get();
         }
 
-        [HttpGet]
-        [Route("Test")]
-        public object Test()
-        {
-            return new { Connection = Settings.ConnectionString };
-        }
-    }
 
+
+        [HttpPost]
+        [Route("employees")]
+        public ICommandResults Post([FromBody] CreateEmployeeCommand command)
+        {
+            var result = (ICommandResults)_handler.Handler(command);
+            if (_handler.IsValid == false)
+                return result;
+            return result;
+        }
+
+    }
 }
