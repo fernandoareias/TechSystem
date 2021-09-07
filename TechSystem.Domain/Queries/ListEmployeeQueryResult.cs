3a6954249bd1f3a785999cdddf75888a8e73249b
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 11:24:28 -0300

    Add List for Dependents Query Results

--- TechSystem.Domain/Queries/ListEmployeeQueryResult.cs
+++ TechSystem.Domain/Queries/ListEmployeeQueryResult.cs
@@ -1,13 +1,16 @@
-
+using System;
+using System.Collections.Generic;
 
 namespace TechSystem.Domain.Queries
 {
     public class ListEmployeeQueryResults
     {
+
         public System.Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Wage { get; set; }
         public int Gender { get; set; }
         public int Role { get; set; }
+        public List<ListDependentsQueryResults> Dependents { get; set; }
     }
 }
