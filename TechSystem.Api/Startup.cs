f8a6675608a9873653a49a36d0cc37d047ab340a
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 11:26:07 -0300

    DI, Add MVC Core

--- TechSystem.Api/Startup.cs
+++ TechSystem.Api/Startup.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using TechSystem.Infra.DataContext;
+using TechSystem.Domain.Repositories.Contracts;
+using TechSystem.Infra.Repositories;
+using TechSystem.Domain.Handlers;
+using TechSystem.Shared;
 
 namespace TechSystem.Api
 {
@@ -14,8 +16,29 @@ namespace TechSystem.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public static IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
+            // Carrega o appsettings no builder
+            var builder = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json");
+            // Seta a configuração
+            Configuration = builder.Build();
+            // Pega a connection string do appsettings.json
+            Settings.ConnectionString = $"{Configuration["connectionStrings"]}";
+            System.Console.WriteLine(Settings.ConnectionString);
+
+
+            // Carrega o MVC Core (Controllers, Rotas e CoreServiecs)
+            services.AddMvcCore();
+
+            // Injeção de Dependências.
+            services.AddScoped<TechDataContext, TechDataContext>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<EmployeeHandler, EmployeeHandler>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +51,8 @@ namespace TechSystem.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            // Seta os controllers como endpoints
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
