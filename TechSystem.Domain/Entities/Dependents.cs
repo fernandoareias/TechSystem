70d8c971eff390a8cc68af2b4528c388f313a3cb
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 18:19:43 -0300

    Dependents Model

--- TechSystem.Domain/Entities/Dependents.cs
+++ TechSystem.Domain/Entities/Dependents.cs
@@ -9,9 +9,18 @@ namespace TechSystem.Domain.Entities
 {
     public class Dependents : Entity
     {
+        public Dependents() { }
+        public Dependents(Name name, Document document, System.Guid employee)
+        {
+            Name = name;
+            Document = document;
+            EmployeeId = employee;
+        }
+
         public Name Name { get; private set; }
         public Document Document { get; private set; }
 
+        public System.Guid EmployeeId { get; private set; }
 
 
     }
