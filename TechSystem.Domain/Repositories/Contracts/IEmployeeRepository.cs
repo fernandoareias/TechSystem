b26f818d393693728b2d20a663f07ca1bdefb8dc
fernandoareias <nando.calheirosx@gmail.com>
2021-09-07 18:23:10 -0300

    Employee Repository

--- TechSystem.Domain/Repositories/Contracts/IEmployeeRepository.cs
+++ TechSystem.Domain/Repositories/Contracts/IEmployeeRepository.cs
@@ -8,8 +8,13 @@ namespace TechSystem.Domain.Repositories.Contracts
 {
     public interface IEmployeeRepository
     {
-        void Save(Employee employee);
+        void Create(Employee employee);
+        void Update(Employee employee);
+        void Delete(Employee employee);
+        IEnumerable<ListEmployeeAndDependentsQueryResults> GetListEmployeesAndDependents();
         IEnumerable<ListEmployeeQueryResults> Get();
+
+        ListEmployeeQueryResults GetById(System.Guid Id);
 
         bool CheckEmail(string email);
     }
