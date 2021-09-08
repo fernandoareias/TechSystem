8bb9ddcfbadb82f89aaa7cfeb7f819d9a6a37534
fernandoareias <nando.calheirosx@gmail.com>
2021-09-08 17:38:37 -0300

    Command for update employee

---
+++ TechSystem.Domain/Commands/EmployeeCommands/Inputs/UpdateEmployeeCommand.cs
@@ -0,0 +1,43 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace TechSystem.Domain.Commands.EmployeeCommands.Inputs
+{
+    public class UpdateEmployeeCommand : Notifiable<Notification>, ICommands
+    {
+        public UpdateEmployeeCommand() { }
+        public UpdateEmployeeCommand(string firstName, string lastName, decimal wage, int gender, int role)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Wage = wage;
+            Gender = gender;
+            Role = role;
+        }
+
+        public System.Guid Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Wage { get; set; }
+        public int Gender { get; set; }
+        public int Role { get; set; }
+        public void SetId(System.Guid id) => Id = id;
+        public bool Validate()
+        {
+            AddNotifications(
+                new Contract<Notification>()
+                .Requires()
+                .IsGreaterThan(FirstName, 2, "FirstName", "Nome deve ser maior que 2 caractéres.")
+                .IsLowerThan(FirstName, 80, "FirstName", "Nome deve ser menor que 80 caractéres.")
+                .IsGreaterThan(LastName, 2, "LastName", "Sobrenome deve ser maior que 2 caractéres.")
+                .IsLowerThan(LastName, 80, "LastName", "Sobrenome deve ser menor que 80 caractéres.")
+                .IsGreaterThan(Wage, 0, "Wage", "Salário deve ser maior que zero.")
+                .IsGreaterThan(Gender, 0, "Gender", "Genero deve ser maior que zero.")
+                .IsLowerThan(Gender, 8, "Gender", "Gender deve ser menor que 8.")
+                .IsGreaterThan(Role, 0, "Role", "Cargo deve ser maior que zero.")
+                .IsLowerThan(Role, 4, "Role", "Cargo deve ser menor que 4.")
+            );
+            return IsValid;
+        }
+    }
+}
